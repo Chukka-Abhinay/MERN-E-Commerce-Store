c039931aeb14cda4bce1824e81284435fab1f782
Chukka-Abhinay <chukkaahinay123@gmail.com>
2025-08-26 18:01:31 +0530

    file chacnge

--- backend/index.js
+++ backend/index.js
@@ -52,8 +52,8 @@ app.get("/api/config/paypal" , (req,res) =>{
   res.send({clientId: process.env.PAYPAL_CLIENT_ID})
 })
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname , "/uploads")));
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname , "/uploads")));
 
 app.listen(port , () => console.log(`server is running on port : ${port}`)
 );
